a4f7fb0578d6030efcf8667d5244202154905b82
antataiv <antata_iv@abv.bg>
2016-03-14 22:00:22 +0200

    add PlayerMovement, ShipMovement, Shooting scripts

---
+++ Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    //Camera playerCamera;
+    public float walkGravity = 9.8f;
+    public float jumpGravity = 40f;
+    public float moveSpeed = 25f;
+    public float mouseSensitivity = 100f;
+    public float jumpForce = 10f;
+
+    private Rigidbody playerRigidbody;
+    private bool isOnTheGround = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        //playerCamera = Camera.main;
+        SetGravity(walkGravity);
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moving player
+        playerRigidbody.velocity = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal") * moveSpeed, playerRigidbody.velocity.y, Input.GetAxis("Vertical") * moveSpeed));
+
+        //rotate
+        transform.Rotate(new Vector3(0f, Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity, 0f));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        Debug.Log("Jumping...");
+        if (isOnTheGround)
+        {
+            return;
+        }
+
+        isOnTheGround = false;
+        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        SetGravity(jumpGravity);
+    }
+
+    private void SetGravity(float gravityToBeSet)
+    {
+        Physics.gravity = -Vector3.up * gravityToBeSet;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Floor" && !isOnTheGround)
+        {
+            isOnTheGround = true;
+            SetGravity(walkGravity);
+        }
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "Floor" && !isOnTheGround)
+        {
+            isOnTheGround = true;
+
+            if (Physics.gravity != Vector3.up * walkGravity)
+            {
+                SetGravity(walkGravity);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Floor" && isOnTheGround)
+        {
+            isOnTheGround = false;
+            SetGravity(jumpGravity);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject bulletShootPosition;
+    public GameObject bulletObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = GameObject.Instantiate(bulletObject);
+            bullet.gameObject.SetActive(true);
+            bullet.transform.position = bulletShootPosition.transform.position;
+            bullet.transform.rotation = bulletShootPosition.transform.rotation;
+            bullet.AddComponent<BulletShooter>();
+        }
+    }
+}
