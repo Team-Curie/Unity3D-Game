13d02fd0b9d2fd44390e5cd06db421b6df440d7d
AlexKrumov <alex.krumov@mail.com>
2016-03-17 12:21:17 +0200

    When you are new planet press E
    ako imate vaprosi pishete na skype

---
+++ Assets/Scripts/planet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planet : MonoBehaviour {
+
+	public int range = 400;
+	public GameObject player;
+	public float distance;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distanceCheck ();
+	}
+
+	void distanceCheck(){
+		// Distance between Player and Planet
+		distance = Vector3.Distance (this.transform.position, player.transform.position);
+
+		if (distance <= range) {
+			Debug.Log ("You are Near " + this.gameObject.name);
+			player.GetComponent<ship> ().nearPlanet = true;
+		} else {
+			player.GetComponent<ship> ().nearPlanet = false;
+		}
+	}
+}
---
+++ Assets/Scripts/ship.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ship : MonoBehaviour {
+
+	public int moveSpeed = 100;
+	public int mouseSens = 20;
+	public bool nearPlanet = false;
+	public string nearPlanetName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shipController ();
+		landPlanet ();
+	}
+
+	void shipController(){
+		transform.position += transform.forward * Time.deltaTime * moveSpeed * Input.GetAxis("Vertical");
+		//this.GetComponent<Rigidbody>().AddForce(Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed * transform.forward);
+		transform.localEulerAngles += new Vector3 (-Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSens,Input.GetAxis("Mouse X") * Time.deltaTime * mouseSens,0);
+	}
+
+	void landPlanet(){
+		if(nearPlanet == true && Input.GetKeyDown("e")){
+			Application.LoadLevel (nearPlanetName);
+		}
+	}
+
+	void OnTriggerEnter(Collider target){
+		if(target.gameObject.tag == "planet"){
+			nearPlanetName = target.gameObject.name;
+		}
+	}
+
+	void OnTriggerExit(Collider target){
+		if(target.gameObject.tag == "planet"){
+			nearPlanetName = "";
+		}
+	}
+
+		
+}
